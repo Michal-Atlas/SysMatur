c31ec3bba30a8356974e5f0a2b1de329751baeb5
Michal Atlas <michal.z.atlas@gmail.com>
2021-04-20 15:28:55 +0200

    Refactored Api Namespaces, Scoped UnitOfWork

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,12 +1,12 @@
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Data;
 
-namespace api
+namespace Api
 {
     public class Startup
     {
