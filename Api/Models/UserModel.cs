7616d5ae95e93ad40ff66cd1007324d7238ec3a3
Michal Atlas <michal.z.atlas@gmail.com>
2021-04-21 19:57:04 +0200

    Model has password again and a ToUser method

--- Api/Models/UserModel.cs
+++ Api/Models/UserModel.cs
@@ -1,10 +1,23 @@
+using Data.Objects;
+
 namespace Api.Models
 {
     public class UserModel
     {
+        public UserModel(User user)
+        {
+            Username = user.Username;
+            Email = user.Email;
+        }
+
         public string Username { get; set; }
+        public string Email { get; set; }
         public string PasswordSalt { get; set; }
         public string PasswordHash { get; set; }
-        public string Email { get; set; }
+
+        public User ToUser()
+        {
+            return new() {Username = Username, Email = Email, PasswordHash = PasswordHash, PasswordSalt = PasswordSalt};
+        }
     }
 }
