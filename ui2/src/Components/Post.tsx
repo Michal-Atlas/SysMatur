864ea48cd41ecd6923a31b4f00d87604e2066f67
Michal Atlas <michal.z.atlas@gmail.com>
2021-06-14 20:18:54 +0200

    Prepared Post for overhaul

--- ui2/src/Components/Post.tsx
+++ ui2/src/Components/Post.tsx
@@ -1,16 +1,16 @@
 import {PostModel} from "../Classes/PostModel";
-import {FeedBase} from "../Classes/FeedModel";
+import './Post.css';
 
 const Post = (props: { post: PostModel, feed: string }) => {
     const post = props.post;
-    return (<div className="FeedsViewer"><a href={post.anchor}>
+    return (<div className="Post"><a href={post.anchor}>
         <table>
             <tr>
                 <td>@post["Username"]</td>
                 <td style={{textAlign: "right"}}><p>{props.feed}</p></td>
             </tr>
             <tr>
-                <td>{post.date}</td>
+                <td colSpan={1}>{post.date}</td>
             </tr>
             <tr>
                 <td colSpan={3}>{post.summary}</td>
@@ -19,9 +19,8 @@ const Post = (props: { post: PostModel, feed: string }) => {
             <tr>
                 <td style={{textAlign: "center"}} colSpan={3}>FOTO</td>
                 {
-                    post.image !== "" ?
+                    post.image !== "" &&
                     <td style={{width: "5%", border: "1px solid black"}} rowSpan={2}><img alt={""} src={post.image}/></td>
-                    : <></>
                 }
             </tr>
         </table>
